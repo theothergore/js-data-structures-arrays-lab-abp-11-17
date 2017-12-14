cc21cd81e2511502fb6e64a90de0f4b27a3ad6d4
Fly J Gore <jennifer@theothergore.com>
2017-12-14 20:59:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendDriver(name){
+  drivers.push("Ralph");
+}
+function destructivelyPrependDriver(name){
+  drivers.unshift("Bob");
+}
+function destructivelyRemoveLastDriver(){
+  drivers.pop();
+}
+function destructivelyRemoveFirstDriver(){
+  drivers.shift();
+}
+function appendDriver(name){
+  drivers.splice(0, 2, "Broom");
+}
